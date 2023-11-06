a0f5ba869589c5d9a0b5ca8ba42dd8daa2a9dc8f
PengasKhan <cheesepuff42@gmail.com>
2023-11-06 17:15:23 -0600

    Routes
    Most of Routes, other than PUT routes finished as a first draft

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,17 +6,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  try {
-    const productData = await Product.findAll();
-    res.status(200).json(productData);
-  // be sure to include its associated Category and Tag data
-});
+  const productData = Product.findAll({
+      include: [{model: Category}, 
+        {model: Tag, through: ProductTag, as: 'applicable_tags'}
+      ]});
+
+    res.status(200).json(productData).catch((err) => {
+      console.log(err);
+      res.json(err);
+    })});
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, 
+        {model: Tag, through: ProductTag, as: 'applicable_tags'}
+      ]});
+
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+
+      res.status(200).json(productData).catch((err) => {
+        console.log(err);
+        res.json(err);
+      })});
 
 // create new product
 router.post('/', (req, res) => {
@@ -95,8 +112,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
