c372ebc469f93809a2a228a6b4d11e21ed74ea1a
PengasKhan <cheesepuff42@gmail.com>
2023-11-07 17:13:49 -0600

    Debug route testing

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,7 +36,11 @@ router.post('/', (req, res) => {
   });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
 });
 
 router.delete('/:id', async (req, res) => {
