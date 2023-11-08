4baa2655f1b9b26de2a53b11498da6d1b608a6f0
PengasKhan <cheesepuff42@gmail.com>
2023-11-08 15:44:55 -0600

    final fixes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,7 +63,7 @@ router.delete("/:id", async (req, res) => {
       },
     });
 
-    if (!CategoryData) {
+    if (!categoryData) {
       res.status(404).json({ message: "No category found with this id!" });
       return;
     }
